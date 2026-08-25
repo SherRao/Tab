c3d5fb8dcd2911e0e49192ac6acec51336051ac6
Nausher Rao <nausher.rao@mappedin.ca>
2026-08-25 02:02:32 -0400

    chore: fix commitlint config format

---
+++ commitlint.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore"]],
+    "subject-full-stop": [0, "never"],
+    "header-max-length": [2, "always", 100],
+  },
+};
